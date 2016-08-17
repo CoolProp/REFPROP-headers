2a266e882dc66a480aa619b4bca38ddabba755ab
Ian Bell <ian.h.bell@gmail.com>
2016-08-17 00:53:24 -0600

    Fix typo in squashing of CRT warnings

--- REFPROP_lib.h
+++ REFPROP_lib.h
@@ -427,6 +427,7 @@ extern "C" {
         #ifndef _CRT_SECURE_NO_WARNINGS
         #define _CRT_SECURE_NO_WARNINGS
         #endif
+        #endif
 
         #ifndef NOMINMAX
             #define NOMINMAX
