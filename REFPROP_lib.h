786a8208f722f41fcf16da90e589110b4f1b2f8e
Jorrit Wronski <jowr@ipu.dk>
2017-04-06 11:19:40 +0200

    Handling of error number for Windows, #7 should be fine again, also handles #6 upon merge

--- REFPROP_lib.h
+++ REFPROP_lib.h
@@ -442,6 +442,7 @@ extern "C" {
         #pragma error
     #endif
 
+    #include <sstream>
     #include <string>
     #include <algorithm>
 
@@ -586,14 +587,22 @@ extern "C" {
                     strcpy((char*)refpropdllstring, RP_join_path(shared_library_path, shared_library_name).c_str());
                     RefpropdllInstance = LoadLibrary(refpropdllstring);
                 }
-                DWORD lastError;
-                HMODULE hModule;
-                TCHAR dllPath[_MAX_PATH];
+                std::stringstream msg_stream;
+                std::string msg;
                 if (RefpropdllInstance == NULL){
-                    lastError = GetLastError(); // returns error
+                    msg_stream << GetLastError(); // returns error
+                    msg = msg_stream.str();
                 } else {
+                    TCHAR dllPath[_MAX_PATH];
+                    HMODULE hModule;
                     hModule = GetModuleHandle(refpropdllstring);
                     GetModuleFileName(hModule, dllPath, _MAX_PATH);
+                    #ifndef UNICODE
+                        msg = dllPath;
+                    #else
+                        std::wstring wStr = t;
+                        msg = std::string(wStr.begin(), wStr.end());
+                    #endif
                 }
             #elif defined(__RPISLINUX__)
                 if (shared_library_name.empty()) {
@@ -614,7 +623,7 @@ extern "C" {
             if (RefpropdllInstance == NULL)
             {
                 #if defined(__RPISWINDOWS__)
-                    err = "Could not load REFPROP ("+shared_lib_WIN64+", "+shared_lib_WIN32+"), make sure it is in your system search path. In case you run 64bit and you have a REFPROP license, try installing the 64bit DLL from NIST.";
+                    err = "Could not load REFPROP ("+shared_lib_WIN64+", "+shared_lib_WIN32+") due to error "+ msg +", make sure it is in your system search path. In case you run 64bit and you have a REFPROP license, try installing the 64bit DLL from NIST.";
                 #elif defined(__RPISLINUX__)
                     err = "Could not load REFPROP ("+shared_lib_LINUX+"), make sure it is in your system search path.";
                 #elif defined(__RPISAPPLE__)
