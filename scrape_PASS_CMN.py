c20ed88064d5061d90037fc709433f5ae207dd9c
Ian Bell <ian.bell@nist.gov>
2018-01-16 10:51:13 -0700

    Automate scraping of functions from FORTRAN code, add all functions from REFPROP 10.
    Closes #14
    Closes #15
    Closes #12

---
+++ scrape_PASS_CMN.py
@@ -0,0 +1,38 @@
+"""
+A script to scrape all the function exports from the PASS_CMN.FOR
+
+By Ian Bell, NIST, January 2018
+"""
+import os
+from generate_header import generate_interface_file, generate_function_dict
+
+if __name__=='__main__':
+    generate_interface_file('R:/FORTRAN/PASS_FTN.FOR','REFPROP.PYF',verbose=False,python_exe='python')    
+    funcs = generate_function_dict('REFPROP.PYF')
+    for func in sorted(funcs):
+        print('    X(' + func + ') \\')
+
+    for func in sorted(funcs):
+        string_lengths = []
+        types = []
+        for arg in funcs[func]['argument_list']:
+            if len(arg) == 3:
+                name, _type, size = arg
+            else:
+                name, _type = arg
+                size = 0
+            if _type == 'double *' and size == 0:
+                types.append('DOUBLE_REF')
+            elif _type == 'double *' and size != 0:
+                types.append('double *')
+            elif _type == 'int *' and size == 0:
+                types.append('INT_REF')
+            elif _type == 'int *' and size != 0:
+                types.append('int *')
+            elif _type == 'char *':
+                types.append('char *')
+                string_lengths.append('SIZE_T')
+            else: 
+                print(types)
+                raise ValueError()
+        print('#define '+func+'_ARGS '+','.join(types+string_lengths))
