9005db05a276cc3694ebc55762ddaaa68ddd1a8a
Ian Bell <ian.bell@nist.gov>
2019-05-22 18:15:58 -0600

    Pad strings and array as appropriate

--- main.cpp
+++ main.cpp
@@ -34,13 +34,13 @@ int main()
     SETPATHdll(const_cast<char*>(path.c_str()), 400);
 
     int ierr = 0, nc = 1;
-    char herr[255], hfld[] = "AMMONIA|WATER", hhmx[] = "HMX.BNC", href[] = "DEF";
+    char herr[255], hfld[10000] = "AMMONIA|WATER", hhmx[255] = "HMX.BNC", href[4] = "DEF";
     SETUPdll(nc,hfld,hhmx,href,ierr,herr,10000,255,3,255);
     if (ierr > 0) printf("This ierr: %d herr: %s\n", ierr, herr);
     {
         int ierr = 0;
         char herr[255];
-        double z[] = {1.0}, x[] = {1.0}, y[] = {1.0}, T= 300, p = 101.325, d = -1, dl = -1, dv = -1, h = -1, s = -1, u = -1, cp = -1, cv = -1, q = -1, w = -1;
+        double z[20] = {1.0}, x[20] = {1.0}, y[20] = {1.0}, T= 300, p = 101.325, d = -1, dl = -1, dv = -1, h = -1, s = -1, u = -1, cp = -1, cv = -1, q = -1, w = -1;
         TPFLSHdll(T, p, z, d, dl, dv, x, y, h,s,u,cp,cv,w,q,ierr,herr,255);
         if (ierr > 0) printf("This ierr: %d herr: %s\n", ierr, herr);
         printf("This d: %g mol/L\n", d);
--- other.cpp
+++ other.cpp
@@ -12,13 +12,13 @@ void another_file(const std::string & path, const std::string &DLL_file)
     printf("Loaded refprop (in other.cpp): %s @ address %zu\n", loaded_REFPROP ? "true" : "false", REFPROP_address());
 
     int ierr = 0, nc = 1;
-    char herr[255], hfld[] = "R32.FLD", hhmx[] = "HMX.BNC", href[] = "DEF";
+    char herr[255], hfld[10000] = "R32.FLD", hhmx[255] = "HMX.BNC", href[255] = "DEF";
     SETUPdll(nc,hfld,hhmx,href,ierr,herr,10000,255,3,255);
     if (ierr > 0) printf("Other ierr: %ld herr: %s\n", ierr, herr);
     {
         int ierr = 0;
         char herr[255];
-        double z[] = {1.0}, x[] = {1.0}, y[] = {1.0}, T= 300, p = 101.325, d = -1, dl = -1, dv = -1, h = -1, s = -1, u = -1, cp = -1, cv = -1, q = -1, w = -1;
+        double z[20] = {1.0}, x[20] = {1.0}, y[20] = {1.0}, T= 300, p = 101.325, d = -1, dl = -1, dv = -1, h = -1, s = -1, u = -1, cp = -1, cv = -1, q = -1, w = -1;
         TPFLSHdll(T, p, z, d, dl, dv, x, y, h,s,u,cp,cv,w,q,ierr,herr,255);
         if (ierr > 0) printf("Other ierr: %ld herr: %s\n", ierr, herr);
         printf("Other d: %g mol/L\n", d);
